7528ccf1e5dac78ddeae6ea0b5ea90c0d2f68ac9
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-29 05:59:56 +0300

    Add project files.

---
+++ FirsrApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string? input1 = Console.ReadLine();
+            Console.WriteLine($"Hello, {input1}");
+
+        }
+    }
+}
