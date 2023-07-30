0570afe6ca14ea4386000df9bd8b9fb9aad665f5
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-30 15:22:41 +0300

    do while loop.

---
+++ DoWhileLoop/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DoWhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+            //0 to 9
+            int i = 0;
+            do
+            {
+                Console.Write(i + " ");
+                i++;
+            } while (i < 10);
+            Console.WriteLine();
+
+            //9 to 0
+            int p = 9;
+            do
+            {
+                Console.Write( p + " ");
+                p--;
+            }while (p > 0);
+            Console.WriteLine();
+        }
+    }
+}
