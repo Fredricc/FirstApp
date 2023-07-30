d17409e6729aead867e5f5212a3860afbc3a9dd4
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-30 18:34:38 +0300

    goto loop

---
+++ goto_loop/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace goto_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine("Hello, 1");
+            Console.WriteLine("Hello, 2");
+            Console.WriteLine("Hello, 3!");
+            Console.WriteLine("Hello, 4!");
+            Console.WriteLine("Hello, 5!");
+            Console.WriteLine("Hello, 6!");
+            mylabel:
+            Console.WriteLine("Hello, 7!");
+                ++i;
+            if ( i <= 3)
+            {
+                goto mylabel;
+            }
+            Console.WriteLine("Hello, 8!");
+            Console.WriteLine("Hello, 9!");
+            Console.WriteLine("Hello, 10!");
+            Console.WriteLine("Hello, 11!");
+        }
+    }
+}
